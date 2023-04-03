aaea158b4e2b7df9fe4847b8ec6739582c2ecacd
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-04-03 20:55:06 +0530

    crypto app

---
+++ employee-archive/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Employee from './components/Employee';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Employee/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
